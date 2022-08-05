c24672251613a312b35e77c5bd4c149dc8a189d4
dmitriypavlov <dimanikus0106@gmail.com>
2022-08-05 21:53:56 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Input } from "components/Input";
+
+const App = () => {
+  return (
+    <div className="bg-blue-300 flex flex-col min-h-full">
+      <Input />
+    </div>
+  );
+}
+
+export default App;
